46c8679f7608e5688b14c4ebfd3b8584c492fbe9
Quan <roy.nguyen.bc@gmail.com>
2024-09-05 15:25:06 +0700

    bump

--- runtime/src/common/crypto/signature.rs
+++ runtime/src/common/crypto/signature.rs
@@ -44,19 +44,17 @@ static CURVE_ORDER: &[u64] = &[
 ];
 
 /// An Ed25519 private key.
-pub struct PrivateKey(pub ed25519_dalek::Keypair);
+pub struct PrivateKey(pub ed25519_dalek::SigningKey);
 
 impl PrivateKey {
     /// Generates a new private key pair.
     pub fn generate() -> Self {
-        let mut rng = OsRng {};
-
-        PrivateKey(ed25519_dalek::Keypair::generate(&mut rng))
+        PrivateKey(ed25519_dalek::SigningKey::generate(&mut OsRng))
     }
 
     /// Convert this private key into bytes.
     pub fn to_bytes(&self) -> Vec<u8> {
-        let mut bytes = self.0.secret.to_bytes();
+        let mut bytes = self.0.to_bytes();
         let bvec = bytes.to_vec();
         bytes.zeroize();
         bvec
@@ -67,28 +65,26 @@ impl PrivateKey {
     /// # Panics
     ///
     /// This method will panic in case the passed bytes do not have the correct length.
-    pub fn from_bytes(mut bytes: Vec<u8>) -> PrivateKey {
-        let secret = ed25519_dalek::SecretKey::from_bytes(&bytes).unwrap();
-        bytes.zeroize();
-        #[allow(clippy::needless_borrow)]
-        let public = (&secret).into();
+    pub fn from_bytes(bytes: Vec<u8>) -> PrivateKey {
+        let mut sk = bytes.try_into().unwrap();
+        let secret = ed25519_dalek::SigningKey::from_bytes(&sk);
+        sk.zeroize();
 
-        PrivateKey(ed25519_dalek::Keypair { secret, public })
+        PrivateKey(secret)
     }
 
     /// Generate a new private key from a test key seed.
     pub fn from_test_seed(seed: String) -> Self {
-        let seed = Hash::digest_bytes(seed.as_bytes());
-        let secret = ed25519_dalek::SecretKey::from_bytes(seed.as_ref()).unwrap();
-        #[allow(clippy::needless_borrow)]
-        let pk: ed25519_dalek::PublicKey = (&secret).into();
+        let mut seed = Hash::digest_bytes(seed.as_bytes());
+        let sk = Self::from_bytes(seed.as_ref().to_vec());
+        seed.zeroize();
 
-        PrivateKey(ed25519_dalek::Keypair { secret, public: pk })
+        sk
     }
 
     /// Returns the public key.
     pub fn public_key(&self) -> PublicKey {
-        PublicKey(self.0.public.to_bytes())
+        PublicKey(self.0.verifying_key().to_bytes())
     }
 }
 
